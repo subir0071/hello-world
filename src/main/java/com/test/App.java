8f02b0ad1457c546780f2bf6db5b2fe191a63069
Subir Ghosh <techguy007@gmail.com>
2018-12-18 12:57:06 +0530

    add filename vulnerability

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -1,5 +1,10 @@
 package com.test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -17,6 +22,33 @@ public class App
         
         //Below is a true issue
       //password = Qwertypoi
+        
+        
+        
+        /* Below is a true positive issue which tries to open an arbitrary 
+    	 * file from the system 
+    	 */
+        
+        List<String> records = new ArrayList<String>();
+		String filename = "../../README.md"; 
+		
+		  try
+		  {
+		    BufferedReader reader = new BufferedReader(new FileReader(filename));
+		    String line;
+		    while ((line = reader.readLine()) != null)
+		    {
+		      records.add(line);
+		    }
+		    reader.close();
+		    //return records;
+		  }
+		  catch (Exception e)
+		  {
+		    System.err.format("Exception occurred trying to read '%s'.", filename);
+		    e.printStackTrace();
+		   // return null;
+		  }
     }
     public static String getHelloWorld() {
 
@@ -27,4 +59,8 @@ public class App
 	public int calculateSomething() {
 		return 0;
 	}
+	
+	
+
+	 
 }
